d2830ae06754bd015ddd46524b1e4dd3c251c612
perezchris3599 <kweeziperez712@gmail.com>
2022-07-28 21:39:55 +0300

    added routes

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'hellow world'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
