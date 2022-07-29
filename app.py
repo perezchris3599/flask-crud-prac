120f0ed1a895366cb4e7fe3173bcfeed71c1cb7a
perezchris3599 <kweeziperez712@gmail.com>
2022-07-29 17:02:47 +0300

    extends

--- app.py
+++ app.py
@@ -5,19 +5,39 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+
+    title="Home page"
+    context={
+        'title':title
+    }
+    return render_template('index.html', **context)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+
+    title="about page"
+    context={
+        'title':title
+    }
+    return render_template('about.html', **context)
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+
+    title="login page"
+    context={
+        'title':title
+    }
+    return render_template('login.html', **context)
 
 @app.route('/contacts')
 def contacts():
-    return render_template('contacts.html')
+
+    title="contacts page"
+    context={
+        'title':title
+    }
+    return render_template('contacts.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
