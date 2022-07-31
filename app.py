188a3660f86f81edd7e46841372a7bc3292ee322
perezchris3599 <kweeziperez712@gmail.com>
2022-07-31 11:12:34 +0300

    posts in memory

--- app.py
+++ app.py
@@ -2,13 +2,41 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+posts=[
+    {
+        'id':1,
+        'title': 'what a good day',
+        'content':'to mess around with flask'
+
+    },
+     {
+        'id':2,
+        'title': 'focus',
+        'content':'your goals are limitless'
+
+    },
+     {
+        'id':3,
+        'title': 'speech',
+        'content':'feel free to talk'
+
+    },
+     {
+        'id':4,
+        'title': 'IT',
+        'content':'hard asf'
+
+    }, 
+    
+]
 @app.route('/')
 @app.route('/index')
 def index():
 
     title="Home page"
+    
     context={
-        'title':title
+        'title':title,
     }
     return render_template('index.html', **context)
 
